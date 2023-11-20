8c8fb46b65560836d1193518b9d07beb3999e038
Reiwil Lugo <reiwillugo@gmail.com>
2023-11-20 11:36:40 -0400

    FadeInImage implemented to improve image visualization

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { ActivityIndicator, FlatList, Image, Text } from 'react-native'
 import { styles } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { FadeInImage } from '../components/FadeInImage';
 
 export const HomeScreen = () => {
 
@@ -22,8 +23,8 @@ export const HomeScreen = () => {
         keyExtractor={ (pokemon) => pokemon.id }
         showsVerticalScrollIndicator = { false }
         renderItem={({item}) => (
-          <Image
-            source={{ uri: item.picture }}
+          <FadeInImage
+            uri= {item.picture}
             style={{
               width: 100,
               height: 100,
@@ -40,7 +41,7 @@ export const HomeScreen = () => {
           <ActivityIndicator 
             style={{ height: 100 }}
             size={ 20 }
-            color={'red'}
+            color={'gray'}
           />
         )}
       />
